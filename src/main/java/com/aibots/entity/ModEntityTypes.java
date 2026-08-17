a8e8120f7c93dfda4d43307c5e3c9d97bc61c325
teamkiller1200 <thisadressishacked@gmail.com>
2026-08-17 09:34:14 +0900

    Replace deprecated FabricEntityTypeBuilder with vanilla EntityType.Builder

--- src/main/java/com/aibots/entity/ModEntityTypes.java
+++ src/main/java/com/aibots/entity/ModEntityTypes.java
@@ -3,11 +3,9 @@ package com.aibots.entity;
 import com.aibots.Aibots;
 
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 
@@ -18,9 +16,9 @@ public final class ModEntityTypes {
     public static final EntityType<AiluuEntity> AILUU = Registry.register(
         BuiltInRegistries.ENTITY_TYPE,
         ResourceLocation.fromNamespaceAndPath(Aibots.MOD_ID, "ailuu"),
-        FabricEntityTypeBuilder.create(MobCategory.CREATURE, AiluuEntity::new)
-            .dimensions(EntityDimensions.scalable(0.6F, 1.8F))
-            .build()
+        EntityType.Builder.of(AiluuEntity::new, MobCategory.CREATURE)
+            .sized(0.6F, 1.8F)
+            .build("ailuu")
     );
 
     public static void registerAll() {
